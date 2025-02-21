65d9f8dabe9581343c89d9858615a644b48d1748
sakuramioka <lnaresh2006@gmail.com>
2025-02-22 00:13:38 +0530

    More icons and buttons

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ from colorama import init as colorama_init
 from termcolor import colored
 from PIL import Image, ImageTk, ImageColor
 from shared.transforms import RGBTransform
+from datetime import datetime
 # -------------------End-------------------
 
+print(colored(" START ", 'light_grey', 'on_dark_grey'), "Execution Timestamp:", colored(datetime.now(), 'dark_grey'))
+
+# ------------------Theme------------------
 colorama_init() # Initialize colorama for pretty printing
-primary_color = '#00aeef' # Primary color of UI
+selected_color_theme = "blue" # This is where the magic happens
 customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+default_themes = {"blue":'#3B8ED0', "green":'#2CC985', "dark-blue":'#3A7EBF'}
+if selected_color_theme in default_themes:
+    customtkinter.set_default_color_theme(selected_color_theme)  # Default Themes: "blue" (standard), "green", "dark-blue"
+    primary_color = default_themes[selected_color_theme] # Primary color of UI
+    print(colored(" THEME ", 'white', 'on_magenta'),  f"Sucessfully Loaded: '{selected_color_theme.title()}' from default themes list.")
+else:
+    customtkinter.set_default_color_theme(f"themes\\{selected_color_theme}.json")  # Custom Themes: "themes\\xyz.json"
+    theme_json = json.load(open(f"themes\\{selected_color_theme}.json"))
+    primary_color = theme_json['CTkButton']['fg_color'][0] # Primary color of UI
+    print(colored(" THEME ", 'light_grey', 'on_magenta'),  f"Sucessfully Loaded: '{selected_color_theme.title()}' from path" , colored(f"'themes\\{selected_color_theme}.json'", 'dark_grey'))
+    del theme_json # Why waste memory?
+# --------------=----End------=------------
 
 root = customtkinter.CTk()
 root.title("PreciseDose")
@@ -46,7 +61,7 @@ canvas.create_image(0, 0 ,anchor=tk.NW, image=color_static)
 patient = random.choice(os.listdir("content\\patients")) # Select a random patient
 patient_id = int(patient.rstrip(".json"))
 patient = json.load(open(f"content\\patients\\{patient}")) # Parse JSON to Dictionary
-print(colored(" INFO ", 'black', 'on_yellow'),  f"Selected Patient: {patient['name']} [ID: {patient_id}] from path" , colored(f"'content\\patients\\{patient_id}.json'", 'light_grey', 'on_dark_grey'))
+print(colored(" INFO ", 'black', 'on_yellow'),  f"Selected Patient: {patient['name']} [ID: {patient_id}] from path" , colored(f"'content\\patients\\{patient_id}.json'", 'dark_grey'))
 
 # Update Profile
 canvas.create_text(152, 73, text=patient['name'], font=('Alte Haas Grotesk', 17, 'bold'), fill='Grey30', anchor=tk.NW)
@@ -57,6 +72,28 @@ canvas.create_text(233, 159, text=patient['gender'], font=('Alte Haas Grotesk', 
 canvas.create_text(88, 192, text=patient['case'], font=('Alte Haas Grotesk', 12, 'bold'), fill='Grey30', anchor=tk.NW, justify='left', width=300)
 # ------------------------End------------------------
 
+# -----------------Profile and History Buttons-----------------
+full_profile_icon = customtkinter.CTkImage(light_image=Image.open("assets\\icons\\full_profile.png"),
+                                  dark_image=Image.open("assets\\icons\\full_profile.png"),
+                                  size=(80, 80))
+
+medical_records_icon = customtkinter.CTkImage(light_image=Image.open("assets\\icons\\medicaL_records.png"),
+                                  dark_image=Image.open("assets\\icons\\medicaL_records.png"),
+                                  size=(80, 80))
+
+action_history_icon = customtkinter.CTkImage(light_image=Image.open("assets\\icons\\action_history.png"),
+                                  dark_image=Image.open("assets\\icons\\action_history.png"),
+                                  size=(80, 80))
+
+full_profile_button = customtkinter.CTkButton(master=canvas, image=full_profile_icon, text='Complete\nDetails', font=('Alte Haas Grotesk', 15, 'bold'), compound=tk.TOP, width=120, height=150, corner_radius=12, bg_color='White', border_color='White')
+full_profile_button.place(x=12, y=265)
+
+medical_records_button = customtkinter.CTkButton(master=canvas, image=medical_records_icon, text='Medical\nHistory', font=('Alte Haas Grotesk', 15, 'bold'), compound=tk.TOP, width=120, height=150, corner_radius=12, bg_color='White', border_color='White')
+medical_records_button.place(x=145, y=265)
+
+action_history_button = customtkinter.CTkButton(master=canvas, image=action_history_icon, text='Action\nHistory', font=('Alte Haas Grotesk', 15, 'bold'), compound=tk.TOP, width=120, height=150, corner_radius=12, bg_color='White', border_color='White')
+action_history_button.place(x=277, y=265)
+# -----------------------------End-----------------------------
 
 root.mainloop()
 
